201b65f1af8828773b2e8ecb0361ebeea875fdb2
Stanislaw Cruz <stanycruz@outlook.com>
2021-12-05 15:54:46 -0300

    Implementada autenticação MongoB

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,11 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+
+module.exports = mongoose.connect('mongodb://localhost/todo', {
+    "auth": { "authSource": "admin" },
+    "user": process.env.ROOT_USERNAME,
+    "pass": process.env.ROOT_PASSWORD,
+    "useMongoClient": true
+})
